df25f377c3d53368f8fcdd46d078e9c36e9e61af
Shane McIntosh <shanemcintosh@acm.org>
2024-01-24 08:55:36 -0500

    Exercise upload

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # CS489-w24-exercise2
+
+See handout for exercise details.
---
+++ c/input.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "input.h"
+#include "config.h"
+
+/*
+ * /* Get the working directory from user input in a very insecure fashion
+ */
+char *get_dir() {
+  char *dir = calloc(PATH_MAXLEN, sizeof(char));
+  if (!dir) {
+    fprintf(stderr, "Out of memory!");
+  } else {
+    printf("Enter the path to the directory: ");
+    scanf("%s", dir);
+  }
+
+  return dir;
+}
